002c46f43aa197005b02b5a6e7c15c343a4e9404
Anatoly Obukhov <toll1605@gmail.com>
2022-02-19 14:51:44 +0100

    Logs

--- src/main/java/com/example/emul/beans/StartupListener.java
+++ src/main/java/com/example/emul/beans/StartupListener.java
@@ -2,6 +2,8 @@ package com.example.emul.beans;
 
 import com.example.emul.runnables.AliveThread;
 import com.example.emul.util.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +15,7 @@ import java.io.IOException;
 
 @Component
 public class StartupListener {
+    private Logger log = LoggerFactory.getLogger(StartupListener.class);
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
@@ -23,11 +26,14 @@ public class StartupListener {
 
     @EventListener
     public void onApplicationEvent(ContextRefreshedEvent event) throws IOException {
+        log.info("Application started");
         String messageBody = FileUtils.getStringFromFile(String.format("static/startup_%s.json", classifierName));
+        log.info(messageBody);
         rabbitTemplate.convertAndSend("GNUMAExchange", "Classifier.distilbert", messageBody, message -> {
             message.getMessageProperties().setHeader("event", "ClassifierStart");
             return message;
         });
+        log.info("Startup message sent");
         new AliveThread("aliveThread", rabbitTemplate, address);
     }
 }
